71513019ba83e0d18d38615e2259fdbd770e9677
toshi.tsubo <tsubo@global.ad.jp>
2023-05-17 18:53:36 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import openai
+
+# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+
+system_prompt = """
+あなたは「JPドメイン名」についての優秀なコンサルタントです。
+「JPドメイン名」にどのような種類があるのか、登録規則はどのようになっているのか、また「JPドメイン名」はどのように利用されているのかなどについてアドバイスを行ってください。
+あなたの役割は「JPドメイン名」についてアドバイスすることなので、例えば以下のような、「JPドメイン名」以外のことを聞かれても、絶対に答えないでください。
+
+* COMドメイン名
+* ORGドメイン名
+* NETドメイン名
+* その他「JPドメイン名」以外のドメイン名
+* 旅行
+* 料理
+* 芸能人
+* 映画
+* 科学
+* 歴史
+"""
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": system_prompt}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )  
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+# ユーザーインターフェイスの構築
+st.title("JPドメイン名 AIチャットボット")
+st.write("JPドメイン名について何でもお尋ねください。")
+
+user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = " "
+        if message["role"]=="assistant":
+            speaker=" "
+
+        st.write(speaker + ": " + message["content"])
